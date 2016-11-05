0a53d12e7236fa3bbb5d8a7a4b5d352969433b8d
Billnguyenh <billn@uci.edu>
2016-11-04 19:11:55 -0700

    11/4 7pm

--- js/myScript.js
+++ js/myScript.js
@@ -15,16 +15,7 @@ function hideDiv(id) {
   element(id).style.display = "none";
 }
 
-function positionDiv(id, left, top) {
-  var div = element(id);
-  div.style.position = "absolute";
-  div.style.left = left + "px";
-  div.style.top = top + "px";
-}
-
-
-
-function initializePage (){
+function main(){
 
 }
 
